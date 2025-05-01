35d74a72b6010cea6d218d0bfdbfca07e14f370c
Afrinaf <107719683+Afrinaf@users.noreply.github.com>
2025-05-01 22:21:31 +0530

    Delete BackEnd directory

--- BackEnd/routes/visitRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getVisits } from '../controllers/visitController.js';
-
-const router = express.Router();
-
-router.route('/').get(getVisits);
-
-export default router;
